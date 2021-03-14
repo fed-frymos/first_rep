7c779df4be6903fd309fe289996717626cb6150b
fed_frymos <fed-frymos@ukr.net>
2021-03-14 16:20:17 +0200

    js1

---
+++ JS1/app.js
@@ -0,0 +1,10 @@
+let age=26;
+console.log('vozrast ', age+12);
+let car='bmw';
+let driver='vaxtang';
+let driverInfo=`vodila: ${driver}, vozrast: ${age}, 
+mashina vodili: ${car}.`;
+console.log(driverInfo);
+console.log(typeof age);
+let isDoroga=true;
+console.log(typeof isDoroga);
